68c4842ee806a123d977427a5a1d51643c4f669d
Orvin <orvin.tritama@hotmail.com>
2022-11-22 21:36:55 -0800

    make working to username and pw RosenA

--- Website/server.py
+++ Website/server.py
@@ -6,15 +6,14 @@ app = Flask(__name__)
 def home():
    return render_template('login.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-   #  error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-           return redirect(url_for('index'))
-            # error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('login'))
+   error = None
+   if request.method == 'POST':
+      if request.form['username'] != 'RosenA' or request.form['password'] != 'RosenA':
+         return home()
+      else:
+         return redirect(url_for('index'))
  
 
 @app.route('/index')
