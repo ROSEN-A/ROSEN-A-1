14ea47b25b74158ba92468af7bf1cf99bf8ba0d4
Orvin <orvin.tritama@hotmail.com>
2022-11-22 21:25:53 -0800

    added functionality for login

--- Website/server.py
+++ Website/server.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+   return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   #  error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+           return redirect(url_for('index'))
+            # error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('login'))
+ 
+
+@app.route('/index')
+def index():
    return render_template('index.html')
 
 @app.route('/image')
 def image():
    return render_template('image.html')
-
-@app.route('/index')
-def index():
-   return home()
-
 
 if __name__ == '__main__':
    app.run()
