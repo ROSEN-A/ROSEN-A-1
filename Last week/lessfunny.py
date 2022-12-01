2e63b271d8cd9c149d68f14f3e9fbc82ae9c0ae5
Orvin <orvin.tritama@hotmail.com>
2022-12-01 14:23:25 -0800

    lol

--- Last week/lessfunny.py
+++ Last week/lessfunny.py
@@ -1,2 +1,4 @@
 print('hello why are you here?')
 from flask import Flask, render_template, redirect, url_for, request
+print('we need to refactor!!!!')
+from flask import Flask, render_template, redirect, url_for, request
