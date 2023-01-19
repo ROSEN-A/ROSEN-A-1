3a9a905bf320b7343a59ddfab910318d79bffb35
qhu217 <qhu12@student.ubc.ca>
2023-01-18 23:20:42 -0800

    Update connect_server.py

--- VectorDatabase/connect_server.py
+++ VectorDatabase/connect_server.py
@@ -1,6 +1,7 @@
 # ==== milvus libraries ====
 import os
 from os import getcwd
+from datetime import datetime
 
 from pymilvus import connections, Collection, FieldSchema, DataType, CollectionSchema, utility
 # ==== milvus libraries ====
@@ -80,7 +81,7 @@ model = keras.applications.VGG16(weights="imagenet", include_top=True, pooling="
 model_fc2 = Model(inputs=model.input, outputs=model.get_layer("fc2").output)
 
 # create a folder for storing frames
-path = os.getcwd() + "/frames"
+path = os.getcwd() + "/frames" # get current working dir
 os.mkdir(path)
 
 # function to convert a video to a series of images
@@ -92,7 +93,7 @@ def extractImages(pathIn):
     while success:
         vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))  # added this line
         success, image = vidcap.read()
-        print('Read a new frame: ', success)
+        # print('Read a new frame: ', success)
         if not success:
             break
         cv2.imwrite(os.path.join(path, "frame" + str(count) + ".jpg"), image)  # save frame as JPEG file
@@ -103,31 +104,62 @@ extractImages("video.mp4")
 
 
 # function to convert image to vector of 4096 dimensions
-def get_vector(img):
+def getVector(img):
     # initial image dimensions
-    print(img.shape)
+    # print(img.shape)
     # resizing and reshaping image to fit input shape
     img = cv2.resize(img, (224, 224)).reshape(1, 224, 224, 3)
     # resized and reshaped dimensions
-    print(img.shape)
+    # print(img.shape)
     # retrieving vector for image
     vector = model_fc2.predict(img)
     return vector
 
+# function to vectorize frames and store them into the database
+def vectorizeFrames(pathIn):
+    for frames in os.listdir(pathIn):
+        if frames.endswith("jpg"):
+            # vectorize frame
+            framePath = os.path.join(pathIn, os.path.basename(frames))
+            frameName = os.path.abspath(framePath) # frame path
+            frameTimeStamp = datetime.now().strftime("%H:%M:%S") # frame timestamp
+            frameSize = os.stat(frameName).st_size / 1000 # frame size in KB
+            frameLabel = False # if this frame is labelled
+            frameVector = getVector(cv2.imread(frameName)) # frame vector
+            # insert vectors into the DB
+            frameData = [
+                [frameName],
+                [frameTimeStamp],
+                [frameSize],
+                [frameLabel],
+                frameVector]
+            collection.insert(frameData)
+            print(fmt.format(os.path.basename(frames) + " vector inserted into `image`"))
+    return
+
+vectorizeFrames(path)
 
 # load image
-img = cv2.imread("fish.png")
+sampleImage = cv2.imread("fish.jpg")
+# image name
+imageName = os.path.abspath("fish.jpg")
+# image timestamp
+imageTimeStamp = datetime.now().strftime("%H:%M:%S")
+# image size in KB
+imageSize = os.stat(imageName).st_size / 1000
+# image label
+imageLabel = False
 # vectorize image
-vector = get_vector(img)
+imageVector = getVector(sampleImage)
 
 data = [
-    ["some_path"],
-    ["some_timestamp"],
-    [1.5],
-    [True],
-    vector]
+    [imageName],
+    [imageTimeStamp],
+    [imageSize],
+    [imageLabel],
+    imageVector]
 collection.insert(data)
-print(fmt.format("Vector inserted into `image`"))
+print(fmt.format("Sample image vector inserted into `image`"))
 
 print(fmt.format("Drop collection `image`"))
 utility.drop_collection("image")
