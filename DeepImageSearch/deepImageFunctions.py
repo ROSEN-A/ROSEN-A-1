36c78df60aa6d22166059301e65befe980437849
Orvin <orvin.tritama@hotmail.com>
2023-01-20 12:24:00 -0800

    all functions for deepImageSearch

--- DeepImageSearch/deepImageFunctions.py
+++ DeepImageSearch/deepImageFunctions.py
@@ -60,3 +60,79 @@ class FeatureExtractor:
                 features.append(None)
                 continue
         return features
+    
+class Index:
+    def __init__(self,image_list:list):
+        self.image_list = image_list
+        if 'meta-data-files' not in os.listdir():
+            os.makedirs("meta-data-files")
+        self.FE = FeatureExtractor()
+    def start_feature_extraction(self):
+        image_data = pd.DataFrame()
+        image_data['images_paths'] = self.image_list
+        f_data = self.FE.get_feature(self.image_list)
+        image_data['features']  = f_data
+        image_data = image_data.dropna().reset_index(drop=True)
+        image_data.to_pickle(config.image_data_with_features_pkl)
+        print("Image Meta Information Saved: [meta-data-files/image_data_features.pkl]")
+        return image_data
+    def start_indexing(self,image_data):
+        self.image_data = image_data
+        f = len(image_data['features'][0]) # Length of item vector that will be indexed
+        t = AnnoyIndex(f, 'euclidean')
+        for i,v in tqdm(zip(self.image_data.index,image_data['features'])):
+            t.add_item(i, v)
+        t.build(100) # 100 trees
+        print("Saved the Indexed File:"+"[meta-data-files/image_features_vectors.ann]")
+        t.save(config.image_features_vectors_ann)
+    def Start(self):
+        if len(os.listdir("meta-data-files/"))==0:
+            data = self.start_feature_extraction()
+            self.start_indexing(data)
+        else:
+            print("Metadata and Features are allready present, Do you want Extract Again? Enter yes or no")
+            flag  = str(input())
+            if flag.lower() == 'yes':
+                data = self.start_feature_extraction()
+                self.start_indexing(data)
+            else:
+                print("Meta data allready Present, Please Apply Search!")
+                print(os.listdir("meta-data-files/"))
+
+class SearchImage:
+    def __init__(self):
+        self.image_data = pd.read_pickle(config.image_data_with_features_pkl)
+        self.f = len(self.image_data['features'][0])
+    def search_by_vector(self,v,n:int):
+        self.v = v # Feature Vector
+        self.n = n # number of output 
+        u = AnnoyIndex(self.f, 'euclidean')
+        u.load(config.image_features_vectors_ann) # super fast, will just mmap the file
+        index_list = u.get_nns_by_vector(self.v, self.n) # will find the 10 nearest neighbors
+        return dict(zip(index_list,self.image_data.iloc[index_list]['images_paths'].to_list()))
+    def get_query_vector(self,image_path:str):
+        self.image_path = image_path
+        img = Image.open(self.image_path)
+        fe = FeatureExtractor()
+        query_vector = fe.extract(img)
+        return query_vector
+    def plot_similar_images(self,image_path:str):
+        self.image_path = image_path
+        query_vector = self.get_query_vector(self.image_path)
+        img_list = list(self.search_by_vector(query_vector,16).values())
+        # Visualize the result
+        axes=[]
+        fig=plt.figure(figsize=(20,15))
+        for a in range(4*4):
+            axes.append(fig.add_subplot(4, 4, a+1))  
+            plt.axis('off')
+            plt.imshow(Image.open(img_list[a]))
+        fig.tight_layout()
+        fig.suptitle('Similar Result Found', fontsize=22)
+        plt.show(fig)
+    def get_similar_images(self,image_path:str,number_of_images:int):
+        self.image_path = image_path
+        self.number_of_images = number_of_images
+        query_vector = self.get_query_vector(self.image_path)
+        img_dict = self.search_by_vector(query_vector,self.number_of_images)
+        return img_dict
