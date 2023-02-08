af7a53bf2b142ad39a1431c47b1876d223a3ed21
orvin.tritama <orvin.tritama@hotmail.com>
2023-02-07 20:46:20 -0800

    separated image and video uploaded

--- Website/server.py
+++ Website/server.py
@@ -4,14 +4,22 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+# Config for extensions for video and images
 app.config['PHOTO_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
 app.config['VIDEO_EXTENSIONS'] = ['.mp4']
-app.config['UPLOAD_PATH'] = './static/uploads'
 
+# Config for uploaded images/videos path
+app.config['UPLOAD_PATH_IMAGE'] = './static/uploadedImage'
+app.config['UPLOAD_PATH_VIDEO'] = './static/uploadedVideo'
+
+################################################ LOGIN PAGE ####################################################
+# First rendered papge
 @app.route('/')
 def home():
    return render_template('login.html')
 
+# Login redirect to uploadImage() which renders uploadImage.html
 @app.route('/', methods=['GET', 'POST'])
 def login():
    if request.method == 'POST':
@@ -20,11 +28,16 @@ def login():
       else:
          return redirect(url_for('uploadImage'))
 
+
+################################################ UPLOAD IMAGE PAGE ####################################################
+
+# Setup the config to UPLOAD_PATH_IMAGE and renders uploadImage.html
 @app.route('/uploadImage')
 def uploadImage():
-    files = os.listdir(app.config['UPLOAD_PATH'])
+    files = os.listdir(app.config['UPLOAD_PATH_IMAGE'])
     return render_template('uploadImage.html', files=files)
 
+# Get the file name of the uploaded file and save to ./static/uploadedImage folder
 @app.route('/uploadImage', methods=['POST'])
 def upload_files():
     uploaded_file = request.files['file']
@@ -33,17 +46,22 @@ def upload_files():
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['PHOTO_EXTENSIONS']:
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH_IMAGE'], filename))
     return redirect(url_for('uploadImage'))
 
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+# This method is called in uploadImage.html, loop over the images from UPLOAD_PATH_IMAGE and preview in the screen
+# <img src="{{ url_for('upload_image', filename=file) }}">
+@app.route('/uploadedImage/<filename>')
+def upload_image(filename):
+    return send_from_directory(app.config['UPLOAD_PATH_IMAGE'], filename)
+
+
+################################################ UPLOAD VIDEO  PAGE ####################################################
 
 @app.route('/uploadVideo')
 def uploadVideo():
-   files = os.listdir(app.config['UPLOAD_PATH'])
-   return render_template('uploadVideo.html')
+   files = os.listdir(app.config['UPLOAD_PATH_VIDEO'])
+   return render_template('uploadVideo.html', files=files)
 
 @app.route('/uploadVideo', methods=['POST'])
 def upload_video():
@@ -53,9 +71,17 @@ def upload_video():
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['VIDEO_EXTENSIONS']:
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH_VIDEO'], filename))
     return redirect(url_for('uploadVideo'))
 
+# To be deleted later if not needed
+# To be used similar to upload_image, but this will be called in uploadVideo.html to preview video
+# @app.route('/uploadedVideo/<filename>')
+# def upload_video(filename):
+#     return send_from_directory(app.config['UPLOAD_PATH_VIDEO'], filename)
+
+
+################################################ TO BE ADDED ####################################################
 @app.route('/image')
 def image():
    return render_template('image.html')
