9bcc3a95d04456af5376a2ccd982a5d6bd216436
Orvin <orvin.tritama@hotmail.com>
2023-03-03 23:29:05 -0800

    done

--- Website/deepImageSearch.py
+++ Website/deepImageSearch.py
@@ -9,7 +9,7 @@ import time
 # For Faster Serching we need to index Data first, After Indexing all the meta data stored on the local path
 # added this to skip prompting user. So when user click 'run' in front-end, it will automatically answer yes to the prompt
 
-def imageSearch(referencePathIn, pathIn):
+def imageSearch(referencePathIn, pathIn, numOfImagesWanted):
         image_list = LoadData().from_folder([referencePathIn, pathIn])
         start = time.time();
         indexDir = './meta-data-files'
@@ -17,10 +17,7 @@ def imageSearch(referencePathIn, pathIn):
                 shutil.rmtree(indexDir)
         Index(image_list).Start()
             # for searching, you need to give the image path and the number of the similar image you want
-        ctr = 0
-        for image in os.listdir(pathIn):
-            ctr = ctr + 1
-        print(SearchImage().get_similar_images(image_path=image_list[0],number_of_images=ctr))
+        print(SearchImage().get_similar_images(image_path=image_list[0],number_of_images=numOfImagesWanted))
         end = time.time();
         print(end-start);
 
