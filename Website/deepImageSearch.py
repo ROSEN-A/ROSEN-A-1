ac29bf2bf2ad329ac39a4ed76190d4e1c0a399c5
Orvin <orvin.tritama@hotmail.com>
2023-03-03 23:33:22 -0800

    added

--- Website/deepImageSearch.py
+++ Website/deepImageSearch.py
@@ -10,16 +10,17 @@ import time
 # added this to skip prompting user. So when user click 'run' in front-end, it will automatically answer yes to the prompt
 
 def imageSearch(referencePathIn, pathIn, numOfImagesWanted):
-        image_list = LoadData().from_folder([referencePathIn, pathIn])
-        start = time.time();
-        indexDir = './meta-data-files'
-        if os.path.exists(indexDir):
-                shutil.rmtree(indexDir)
-        Index(image_list).Start()
-            # for searching, you need to give the image path and the number of the similar image you want
-        print(SearchImage().get_similar_images(image_path=image_list[0],number_of_images=numOfImagesWanted))
-        end = time.time();
-        print(end-start);
+    image_list = LoadData().from_folder([referencePathIn, pathIn])
+    start = time.time();
+    indexDir = './meta-data-files'
+    if os.path.exists(indexDir):
+            shutil.rmtree(indexDir)
+    Index(image_list).Start()
+        # for searching, you need to give the image path and the number of the similar image you want
+    imageList = SearchImage().get_similar_images(image_path=image_list[0],number_of_images=numOfImagesWanted))
+    end = time.time();
+    print(end-start);
+    return imageList
 
 # If you want to plot similar images you can use this method, It will plot 16 most similar images from the data index
 # SearchImage().plot_similar_images(image_path = image_list[0])
