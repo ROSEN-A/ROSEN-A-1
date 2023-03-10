eac59039759a8476f9935f97e4be32f44428c782
Fareeha <fareeha.stars@gmail.com>
2023-03-10 11:20:13 -0800

    Enabled video page button disabling function

--- Website/static/javascript/indexPage.js
+++ Website/static/javascript/indexPage.js
@@ -15,16 +15,16 @@ var inputNode = document.querySelector('input')
 inputNode.addEventListener('change', Play_Video, false)
 })()
 
-// window.onload = function() {
-//         var video = document.querySelector("video");
-//         var button = document.querySelector("#run-button");
-//         var previewText = document.querySelector("#dragText");
-//         // console.log(video.src);
-//         if(!video.src) {
-//                 button.setAttribute("disabled", "");
-//                 previewText.style.display = "block";
-//         } else {
-//                 button.removeAttribute("disabled");
-//                 previewText.style.display = "none";
-//         }
-// }
+window.onload = function() {
+        var video = document.querySelector("video");
+        var button = document.querySelector("#run-button");
+        var previewText = document.querySelector("#dragText");
+        // console.log(video.src);
+        if(!video.src) {
+                button.setAttribute("disabled", "");
+                previewText.style.display = "block";
+        } else {
+                button.removeAttribute("disabled");
+                previewText.style.display = "none";
+        }
+}
