a75b0863b191632163d1e1490138c2126a702404
qhu217 <qhu12@student.ubc.ca>
2023-04-19 16:07:56 -0700

    Deleted irrelevant files

--- Last week/lessfunny.py
+++
@@ -1,5 +0,0 @@
-
-print('hello why are you here?')
-from flask import Flask, render_template, redirect, url_for, request
-print('we need to refactor!!!!')
-from flask import Flask, render_template, redirect, url_for, request
--- VectorDatabase/disconnect_server.py
+++
@@ -1,2 +0,0 @@
-from pymilvus import connections
-connections.disconnect("default")
